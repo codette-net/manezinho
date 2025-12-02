ac0c7de9d8ec81a4dc21fb085e4dbf45f0753d84
codette net <info@codette.net>
2025-12-02 13:22:58 +0100

    implemented autoloading

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,12 +4,16 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
+class ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e
 {
     public static $prefixLengthsPsr4 = array (
         'P' => 
         array (
             'PHPMailer\\PHPMailer\\' => 20,
+        ),
+        'C' => 
+        array (
+            'CMSOJ\\' => 6,
         ),
     );
 
@@ -17,6 +21,10 @@ class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
         'PHPMailer\\PHPMailer\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+        'CMSOJ\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/CMSOJ',
         ),
     );
 
@@ -27,9 +35,9 @@ class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e::$classMap;
 
         }, null, ClassLoader::class);
     }
