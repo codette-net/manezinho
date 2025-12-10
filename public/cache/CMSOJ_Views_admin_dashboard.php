4b516d8911e68d131aabf2f3887e4ed652319621
codette net <info@codette.net>
2025-12-10 14:21:51 +0100

    auth controller , account model

--- public/cache/CMSOJ_Views_admin_dashboard.php
+++ public/cache/CMSOJ_Views_admin_dashboard.php
@@ -1,4 +1,5 @@
 <?php class_exists('CMSOJ\Template') or exit; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,23 +17,9 @@
   
 
   
-
-<form action="/admin/login" method="post" class="">
-  <h2>Admin Login</h2>
-  <input type="email" name="admin_email" placeholder="Email" required>
-  <input type="password" name="admin_password" placeholder="Password" required>
-  <input type="submit" value="Login">
-  <?php if (!empty($_SESSION['login_error'])): ?>
-  <div class="error-msg">
-    <?= htmlspecialchars($_SESSION['login_error']) ?>
-  </div>
-  <?php unset($_SESSION['login_error']); ?>
-  <?php endif; ?>
-
-  <p>
-    <a href="/admin/forgot-password">Forgot Password?</a>
-  </p>
-</form>
+<h1>Welcome to Admin </h1>
+<p>working ! <?php echo \CMSOJ\Template::asset($display_name) ?> </p>
+<a href="/admin/logout">logout</a>
 
 
   
